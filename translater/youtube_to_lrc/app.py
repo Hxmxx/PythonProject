fc223cdd8f6abea51bfa7603954924cd4b011750
조하민 <chohamin0611@gmail.com>
2025-05-14 19:22:20 +0900

    브라우저 쿠키 우회

--- translater/youtube_to_lrc/app.py
+++ translater/youtube_to_lrc/app.py
@@ -19,7 +19,7 @@ def download_youtube_audio(url):
             'preferredquality': '192',
         }],
         'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
         }
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
